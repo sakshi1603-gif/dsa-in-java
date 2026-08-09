b828e871600ff63917abd88b6bad5f3eaa156a63
sakshi biradar <biradarsakshi8@gmail.com>
2026-08-09 18:16:57 +0530

    Added solution for search in 2d array II using binary search pattern

--- levelUp/9_LevelUp_Binary_Search/Lecture_3/lect3_2_Search_2D_Matrix2.java
+++ levelUp/9_LevelUp_Binary_Search/Lecture_3/lect3_2_Search_2D_Matrix2.java
@@ -22,3 +22,50 @@
 // All the integers in each row are sorted in ascending order.
 // All the integers in each column are sorted in ascending order.
 // -109 <= target <= 109
+import java.util.*;
+
+public class lect3_2_Search_2D_Matrix2 {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int i = 0;
+        int j = matrix[0].length - 1;
+
+        while (i < matrix.length && j >= 0) {
+
+            if (matrix[i][j] == target) {
+                return true;
+            } 
+            else if (target < matrix[i][j]) {
+                j--;
+            } 
+            else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
+        System.out.println(searchMatrix(matrix, target));
+
+        sc.close();
+    }
+}
